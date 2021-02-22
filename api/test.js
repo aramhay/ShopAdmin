4036a6d0c763bb9b7430b3bb8a3f237e548d2155
aramhay <aramhayrapetyan94@gmail.com>
2021-02-22 19:02:34 +0400

    finished home page sections

--- api/test.js
+++ api/test.js
@@ -1,10 +1,43 @@
-function isSorted(array) {
-  let count = 0;
-const limit = array.length - 1;
-for (let i = 0; i < limit; i++) {
-  const current = array[i], next = array[i + 1];
-  if (current > next) { count+=1,console.log(count)}
-      if (count>1) return false
-}
-return true;
-}
+// function isSorted(array) {
+//   let count = 0;
+// const limit = array.length - 1;
+// for (let i = 0; i < limit; i++) {
+//   const current = array[i], next = array[i + 1];
+//   if (current > next) { count+=1,console.log(count)}
+//       if (count>1) return false
+// }
+// return true;
+// }
+
+// var v = 1
+// var f1 = function () {
+//   console.log(v);
+// }
+
+// var f2 = function () {
+//   var v = 2;
+//   f1();
+// }
+
+// f2();
+
+// class X{
+//   get Y() {return  42} 
+
+// }
+
+// var x = new X ()
+
+// console.log(x.Y);
+// ....................................
+// sum (10,20)
+// diff(10,20)
+
+
+// function sum (x,y){
+//   return x +y 
+// }
+
+// let diff = function (x,y) {
+//   return x - y ;
+// }
